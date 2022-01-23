7b958277969d9e524a75ee1cac7ddb0145f7520d
Daffa Z <67418594+daffaz@users.noreply.github.com>
2022-01-24 00:55:45 +0700

    next checking age

--- mooc-java-programming-i/part01-Part01_34.Same/src/main/java/Same.java
+++ mooc-java-programming-i/part01-Part01_34.Same/src/main/java/Same.java
@@ -5,7 +5,17 @@ public class Same {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        // Write your program here. 
+        
+        System.out.println("Enter the first string:");
+        String first = scan.nextLine();
+        
+        System.out.println("Enter the first string:");
+        String second = scan.nextLine();
+        
+        if (first.equals(second)) {
+            System.out.println("Same");
+        }else {
+            System.out.println("Different");
+        }
     }
 }
