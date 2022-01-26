1605610b1fb302943d99ad30158fd545fc1d973e
Daffa Z <67418594+daffaz@users.noreply.github.com>
2022-01-26 11:10:40 +0700

    part 1-1 complete

--- mooc-java-programming-i/part01-Part01_35.CheckingTheAge/src/main/java/CheckingTheAge.java
+++ mooc-java-programming-i/part01-Part01_35.CheckingTheAge/src/main/java/CheckingTheAge.java
@@ -5,6 +5,14 @@ public class CheckingTheAge {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        
+        System.out.print("How old are you? ");
+        int age = Integer.valueOf(scan.nextLine());
+        
+        if (age > 120 || age < 0) {
+            System.out.println("Impossible!");
+        }else {
+            System.out.println("OK");
+        }
     }
 }
