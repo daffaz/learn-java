5c6424e87a64c23af74970217e194a2bd7d2b8d1
Daffa Z <67418594+daffaz@users.noreply.github.com>
2022-01-28 21:52:15 +0700

    next more loops part 2-3

--- mooc-java-programming-i/part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ mooc-java-programming-i/part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -5,6 +5,24 @@ public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        int positiveCounter = 0;
+        int positiveSum = 0;
+        
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+            
+            if (number == 0) {
+                break;
+            }else if (number > 0) {
+                positiveCounter += 1;
+                positiveSum += number;
+            }
+        }
+        if (positiveCounter < 1) {
+            System.out.println("Cannot calculate the average");
+        } else {
+            System.out.println((positiveSum / (double) positiveCounter));
+        }
     }
 }
